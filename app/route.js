3ac559cdd891916574442930639876c0d13ed26f
scientistmind <luk.ku@hotmail.com>
2020-07-02 22:08:59 +0530

    index modifed

--- app/route.js
+++ app/route.js
@@ -1,16 +1,16 @@
-var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
-  $routeProvider
-  .when("/", {
-    templateUrl : "templates/main.html"
-  })
-  .when("/red", {
-    templateUrl : "templates/red.html"
-  })
-  .when("/blue", {
-    templateUrl : "templates/blue.html"
-  })
-  .when("/green", {
-    templateUrl : "templates/green.html"
-  });
-});
+// var app = angular.module("myApp", ["ngRoute"]);
+// app.config(function($routeProvider) {
+//   $routeProvider
+//   .when("/", {
+//     templateUrl : "templates/main.html"
+//   })
+//   .when("/red", {
+//     templateUrl : "templates/red.html"
+//   })
+//   .when("/blue", {
+//     templateUrl : "templates/blue.html"
+//   })
+//   .when("/green", {
+//     templateUrl : "templates/green.html"
+//   });
+// });
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>angular routing</title>
+    <title>angular controller</title>
 	
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
@@ -11,34 +11,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body ng-app='myApp'>
-<header>
+<p>
+<button type="button" class="btn btn-primary">linux</button>
+<button type="button" class="btn btn-primary">windows</button>
+<button type="button" class="btn btn-primary">macOS</button>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">ROUTING</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#/!">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#!red">Red</a>
-      </li>
-	  <li class="nav-item">
-        <a class="nav-link" href="#!blue">Blue</a>
-      </li>
-	  <li class="nav-item">
-        <a class="nav-link" href="#!green">Green</a>
-      </li>
-      
-    </ul>
-    
-  </div>
-</nav>
-</header>
+</p>
 
 
 <div ng-view></div>
